cb2f99e94f7a3cdb2b8102c75d6552eb436eced6
kelensmu <62132782+kelensmu@users.noreply.github.com>
2020-03-13 09:36:25 +0100

    Update chroot-kiss.sh

--- chroot-kiss.sh
+++ chroot-kiss.sh
@@ -3,5 +3,5 @@ tar xvf kiss-chroot.tar.xz -C /mnt --strip-components 1
 cp quick-kiss.sh /mnt/
 cp quick-kernel.sh /mnt/
 cp quick-grub.sh /mnt/
-chmod +x kiss-chroot.sh
+chmod +x kiss-chroot
 ./kiss-chroot /mnt 
