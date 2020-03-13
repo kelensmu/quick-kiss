7482ad63454a071cabba50da994344282271bb98
kelensmu <62132782+kelensmu@users.noreply.github.com>
2020-03-13 09:32:33 +0100

    Update download-kiss.sh

--- download-kiss.sh
+++ download-kiss.sh
@@ -1,12 +1,4 @@
-echo "Make sure to have Internet and your"
-echo "target mounted with /mnt/boot at /mnt"
-echo "we will unpack for you on it"
-read -p "Press enter to continue..."
-clear
 echo "downloading..."
-echo ""
-sleep 1
-
 url=github.com/kisslinux/repo/releases/download/1.9.11
 wget "$url/kiss-chroot.tar.xz"
 wget "$url/kiss-chroot.tar.xz.sha256"
